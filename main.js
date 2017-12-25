10d87f83f07b570c6aab8a6ba4611b7d788af1ee
Garrett Levine <garrlevine@gmail.com>
2017-12-25 11:14:51 -0500

    created the markup for the form

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+var madlib = `A vacation is when you take a trip to some __________________ place
+with your __________________ family. Usually you go to some place
+that is near a/an __________________ or up on a/an __________________.
+A good vacation place is one where you can ride __________________
+or play __________________ or go hunting for __________________`;
+
+var adjective1 = '';
+var adjective2 = '';
+var noun1 = '';
+var noun2 = '';
+var pluralNoun1 = '';
+var pluralNoun2 = '';
+var game = '';
